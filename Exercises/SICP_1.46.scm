288cbbd19ef2c14f4bc6045dddd9501b1ad14852
Peter Turner <git-commit@flintsteel7.com>
2018-08-01 21:26:33 -0700

    add 8-1 progress

--- Exercises/SICP_1.46.scm
+++ Exercises/SICP_1.46.scm
@@ -1,17 +1,30 @@
-(define (iterative-improve good-enough? improve target)
+(define (iterative-improve good-enough? improve)
   (define (iter result)
-    (if (good-enough? result target)
+    (if (good-enough? result (improve result))
       result
-      (iter (improve result target))))
+      (iter (improve result))))
   (lambda (guess) (iter guess)))
 
 ; rewrite sqrt procedure of 1.1.7 in terms of iterative-improve
 (define (sqrt x)
-  (define (good-enough? guess target)
-    (< (abs (- (* guess guess) target)) 0.001))
-  (define (improve guess target)
-    (/ (+ guess (/ target guess)) 2))
+  (define (good-enough? guess next_guess)
+    (< (abs (- guess next_guess)) 0.00001))
+  (define (improve guess)
+    (/ (+ guess (/ x guess)) 2))
   ((iterative-improve
     good-enough?
-    improve
-    x) 1.0))
+    improve) 1.0))
+
+; rewrite fixed-point procedure of 1.3.3 in terms of iterative-improve
+(define (fixed-point fun first-guess)
+  (define (good-enough? guess next_guess)
+    (< (abs (- guess next_guess))
+      0.00001))
+  (define (try guess)
+    (let ((next (fun guess)))
+      (if (good-enough? guess next)
+          next
+          (try next))))
+  ((iterative-improve
+    good-enough?
+    try) first-guess))
